37208cde6c4fdd17e58d6e524d904d71e21e2daa
Thomas Moseler <tmoseler@gmx.de>
2016-01-01 21:00:06 +0100

    Tommiblog Theme

---
+++ themes/tommiblog/js/tommiblog-custom.js
@@ -0,0 +1,18 @@
+(function ($) {
+  
+  $(document).ready(function(){
+    
+  // Open and close the main mobile menu
+    $('#block-mobilemenuswitcher').click(function(){
+       $('body').toggleClass('mobile-menu-open');
+       
+    });
+    $('#block-mobilemenuswitcher-2').click(function(){
+       $('body').toggleClass('mobile-menu-open');
+       
+    });
+
+       
+  });  // end document.ready
+
+})(jQuery);
