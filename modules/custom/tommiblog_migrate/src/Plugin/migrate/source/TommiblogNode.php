fcbd8ff4607c8a0c95e2ecc262781fdc681d01f1
Thomas Moseler <tmoseler@gmx.de>
2016-12-25 22:00:58 +0100

    Admin Toolbar Modul, Anfang des Node Migrate Modules

---
+++ modules/custom/tommiblog_migrate/src/Plugin/migrate/source/TommiblogNode.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\tommiblog_migrate\Plugin\migrate\source\TommiblogNode.
+ */
+
+namespace Drupal\tommiblog_migrate\Plugin\migrate\source;
+
+/**
+ * Drupal 8 node source from database.
+ *
+ * @MigrateSource(
+ *   id = "tommiblog_node"
+ * )
+ */
+
+class TommiblogNode extends DrupalSqlBase {
+
+}
